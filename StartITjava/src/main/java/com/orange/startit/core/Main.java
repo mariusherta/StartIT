a062bf216632d9b91c672d03ce3e2620b4f95e2e
HERTA Marius Adrian <Marius.Herta@office.orange.intra>
2018-03-21 14:29:48 +0200

    Initial commit

---
+++ StartITjava/src/main/java/com/orange/startit/core/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orange.startit.core;
+
+/**
+ *
+ * @author Marius.Herta
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
